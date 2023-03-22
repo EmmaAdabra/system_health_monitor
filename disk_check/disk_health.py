8a8d2b974e21bc247de2b946e40bcc844580f5cc
EmmaAdabra <adabraemma@gmail.com>
2023-03-22 13:17:29 +0100

    Statement Restructure and a Variable Rename
    Parcent_free_disk renamed to free_disk_parcent because
    the later sound more appropraite.
    The conditional statement in check_disk_space() restructured according
    to the order of how the parameters was passed

--- disk_check/disk_health.py
+++ disk_check/disk_health.py
@@ -8,11 +8,11 @@ def check_disk_space(disk, min_abs, min_parcent):
 	# gets sizes of free, used and total of the disk 
 	disk_stat = shutil.disk_usage(disk)
 	# calculate the parcentage of free disk
-	parcent_free_disk = 100 * (disk_stat.free / disk_stat.total)
+	free_disk_parcent = 100 * (disk_stat.free / disk_stat.total)
 	# calculate the size of free disk in gigabyte
 	free_disk_size = disk_stat.free / 1024**3
 
-	if parcent_free_disk < min_parcent or free_disk_size < min_abs:
+	if free_disk_size < min_abs or free_disk_size < min_parcent:
 		return False
 
 	return True
