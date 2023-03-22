08bedbf10966b1f732063e35491b4af35c2d691e
EmmaAdabra <adabraemma@gmail.com>
2023-03-22 13:56:26 +0100

    Variable Rename and Parameter Naming
    min_abs rename to min_gb, the later best describe
    it function
    The parameters passed to check_disk_space are
    properly name to aid code clearity

--- disk_check/disk_health.py
+++ disk_check/disk_health.py
@@ -3,7 +3,7 @@
 import sys
 import shutil
 
-def check_disk_space(disk, min_abs, min_parcent):
+def check_disk_space(disk, min_gb, min_parcent):
 	''' Returns True if there is enough free disk space and Fale otherwise '''
 	# gets sizes of free, used and total of the disk 
 	disk_stat = shutil.disk_usage(disk)
@@ -12,12 +12,12 @@ def check_disk_space(disk, min_abs, min_parcent):
 	# calculate the size of free disk in gigabyte
 	free_disk_size = disk_stat.free / 1024**3
 
-	if parcent_free_disk < min_parcent or free_disk_size < min_abs:
+	if parcent_free_disk < min_parcent or free_disk_size < min_gb:
 		return False
 
 	return True
 
-if not check_disk_space('/', 2, 10):
+if not check_disk_space(disk='/', min_gb=2, min_parcent=10):
 	print("ERROR: Not enough disk space.")
 	sys.exit(1)
 else:
