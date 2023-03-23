dcbdacecd185501890d95ed90c275c8004e08b76
EmmaAdabra <adabraemma@gmail.com>
2023-03-23 11:16:53 +0100

    disk_health script formatted
    The disk_health.py script was formatted
    to follow PEP 8 coding style

--- disk_check/disk_health.py
+++ disk_check/disk_health.py
@@ -2,23 +2,24 @@
 
 import sys
 import shutil
+import os
+
 
 def check_disk_space(disk, min_gb, min_parcent):
-	''' Returns True if there is enough free disk space and Fale otherwise '''
-	# gets sizes of free, used and total of the disk 
-	disk_stat = shutil.disk_usage(disk)
-	# calculate the parcentage of free disk
-	free_disk_parcent = 100 * (disk_stat.free / disk_stat.total)
-	# calculate the size of free disk in gigabyte
-	free_disk_size = disk_stat.free / 1024**3
+    """Returns True if there is enough free disk space and Fale otherwise"""
+    # gets sizes of free, used and total of the disk
+    disk_stat = shutil.disk_usage(disk)
+    # calculate the parcentage of free disk
+    free_disk_parcent = 100 * (disk_stat.free / disk_stat.total)
+    # calculate the size of free disk in gigabyte
+    free_disk_size = disk_stat.free / 1024**3
 
-	if free_disk_size < min_gb or parcent_free_disk < min_parcent:
-		return False
+    if free_disk_size < min_gb or free_disk_parcent < min_parcent:
+        return False
+    return True
 
-	return True
 
-if not check_disk_space(disk='/', min_gb=2, min_parcent=10):
-	print("ERROR: Not enough disk space.")
-	sys.exit(1)
-else:
-	print("Everything is okay.")
+if not check_disk_space(disk="/", min_gb=2, min_parcent=10):
+    print("ERROR: Not enough disk space.")
+    sys.exit(1)
+print("Everything is ok")
