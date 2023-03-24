5e3dabbed3e498658dc6db4464a7c7ed497a7251
EmmaAdabra <adabraemma@gmail.com>
2023-03-24 14:56:31 +0100

    README.md updated, all_check.py modified
    README.md: updated to capture new features in the script.
    all_check.py: The check_disk_space() modified to return
    True when free disk space is below a certain threshold and False
    if otherwise. Also the 'Not' apprand is remove from the
    conditional statement outside the function

--- scripts/README.md
+++ scripts/README.md
@@ -1,2 +1,3 @@
 # Check for Pending Reboot and Disk Free Space
 
+This script is divided into different functions that check for specific pc health checks
--- scripts/all_check.py
+++ scripts/all_check.py
@@ -5,21 +5,22 @@ import shutil
 import os
 
 
-def check_disk_space(disk, min_gb, min_parcent):
-    """Returns True if there is enough free disk space and False if otherwise"""
+def check_disk_space(disk, min_gb, min_percent):
+    """Returns True if amount of free disk space is less than min_gb or
+    min_percent and False otherwise"""
     # gets sizes of free, used and total of the disk
     disk_stat = shutil.disk_usage(disk)
-    # calculate the parcentage of free disk
-    free_disk_parcent = 100 * (disk_stat.free / disk_stat.total)
+    # calculate the percentage of free disk
+    free_disk_percent = 100 * (disk_stat.free / disk_stat.total)
     # calculate the size of free disk in gigabyte
     free_disk_size = disk_stat.free / 1024**3
 
-    if free_disk_size < min_gb or free_disk_parcent < min_parcent:
-        return False
-    return True
+    if free_disk_size < min_gb or free_disk_percent < min_percent:
+        return True
+    return False
 
 
-if not check_disk_space(disk="/", min_gb=2, min_parcent=10):
+if check_disk_space(disk="/", min_gb=2, min_percent=10):
     print("ERROR: Not enough disk space.")
     sys.exit(1)
 print("Everything is ok")
